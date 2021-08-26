dd4b6dc399b9d2688cb76be2b19b72b117d532b8
chinmayash11 <chinmayash11@gmail.com>
2021-08-26 13:41:15 +0530

    test4

--- github-action-scripts.sh
+++ github-action-scripts.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 sudo apt -y install apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
 sudo rm /var/www/html/index.html
 sudo git clone https://github.com/Chinmayash11/ecomm.git /var/www/html/
+fi
